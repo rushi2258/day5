a85b8b7c6ab02779e86c3677f1ebdbdcce56bf54
rohin <rohineemule@gmail.com>
2022-12-28 20:16:52 +0530

    day5

---
+++ unitconversion1.sh
@@ -0,0 +1,7 @@
+echo "enter a number to be converted"
+read number
+feet=$(($number*12))
+inches=$(($number/12))
+echo "feet conversion to inches="$feet
+echo "42 inches conversion to feet="$inches
+
